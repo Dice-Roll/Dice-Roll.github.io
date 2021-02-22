f86c7f272af33d722b7c4e2501432f11d7ed63e1
itsmerishab <rishabkurapati@yahoo.com>
2021-02-22 16:31:40 +0530

    initial commit

---
+++ DiceRoller/diceroller.js
@@ -0,0 +1,9 @@
+var op;
+function roll(){
+          op = Math.floor(Math.random() * 7);
+          document.getElementById("outputText").innerHTML = op;
+}
+
+if (op = "1") {
+          document.getElementById("dice").style.borderImageSource = "./dice1.png"
+}
